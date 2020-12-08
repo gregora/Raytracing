95b763e20bb983756303ff960c248759033e5f24
Gregor A <gregor.alic@gimb.org>
2020-12-08 20:05:01 +0100

    Add disable controls

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ float * Function(Frame * fr, int x, int y){
 
   if(type == 0){
 
-    test[0] = (fr -> frame[0][x][y]) / (1 + dist);
-    test[1] = (fr -> frame[1][x][y]) / (1 + dist);
-    test[2] = (fr -> frame[2][x][y]) / (1 + dist);
+    test[0] = (fr -> frame[0][x][y]) / (1 + dist*3);
+    test[1] = (fr -> frame[1][x][y]) / (1 + dist*3);
+    test[2] = (fr -> frame[2][x][y]) / (1 + dist*3);
 
     if(dist >= 0.9){
       test[0] = 0.0;
@@ -40,6 +40,7 @@ float * Function(Frame * fr, int x, int y){
 int main(){
 
   bool debug  = true;
+  bool enable_controls = true;
 
   int width, height;
   string file;
@@ -72,19 +73,18 @@ int main(){
   frame.yaw = 0;
   frame.roll = 0;
 
-
   frame.CreateWindow("Raytracing");
 
   int i = 0;
 
   SDL_ShowCursor(SDL_DISABLE);
-  SDL_WarpMouseInWindow(frame.window, frame.width/2, frame.height/2);
+  if(enable_controls) SDL_WarpMouseInWindow(frame.window, frame.width/2, frame.height/2);
 
   float speedz = 0;
 
   while(true){
 
-    SDL_WarpMouseInWindow(frame.window, frame.width/2, frame.height/2);
+    if(enable_controls) SDL_WarpMouseInWindow(frame.window, frame.width/2, frame.height/2);
 
     i++;
     frame.Render();
@@ -101,9 +101,10 @@ int main(){
 
     SDL_GetMouseState(&x, &y);
 
-    if(x < 500) frame.yaw += x - frame.height/2;
-    if(y < 500) frame.pitch -= y - frame.width/2;
-
+    if(enable_controls){
+      if(x < 500) frame.yaw += x - frame.height/2;
+      if(y < 500) frame.pitch -= y - frame.width/2;
+    }
 
     if (frame.pitch > 90 && frame.pitch < 180) frame.pitch = 90;
     if (frame.pitch > 180 && frame.pitch < 270) frame.pitch = 270;
@@ -112,18 +113,21 @@ int main(){
 
     float walking_speed = 10;
 
-    if (keys[SDL_SCANCODE_W]){
-      frame.camera_position = frame.camera_position + Vector(cos(frame.yaw * 3.14 / 180), sin(frame.yaw * 3.14 / 180), 0)*walking_speed;
-    }else if(keys[SDL_SCANCODE_S]){
-      frame.camera_position = frame.camera_position - Vector(cos(frame.yaw * 3.14 / 180), sin(frame.yaw * 3.14 / 180), 0)*walking_speed;
-    }else if(keys[SDL_SCANCODE_A]){
-      frame.camera_position = frame.camera_position - Vector(-sin(frame.yaw * 3.14 / 180), cos(frame.yaw * 3.14 / 180), 0)*walking_speed;
-    }else if(keys[SDL_SCANCODE_D]){
-      frame.camera_position = frame.camera_position + Vector(-sin(frame.yaw * 3.14 / 180), cos(frame.yaw * 3.14 / 180), 0)*walking_speed;
-    }
+    if(enable_controls){
+      if (keys[SDL_SCANCODE_W]){
+        frame.camera_position = frame.camera_position + Vector(cos(frame.yaw * 3.14 / 180), sin(frame.yaw * 3.14 / 180), 0)*walking_speed;
+      }else if(keys[SDL_SCANCODE_S]){
+        frame.camera_position = frame.camera_position - Vector(cos(frame.yaw * 3.14 / 180), sin(frame.yaw * 3.14 / 180), 0)*walking_speed;
+      }else if(keys[SDL_SCANCODE_A]){
+        frame.camera_position = frame.camera_position - Vector(-sin(frame.yaw * 3.14 / 180), cos(frame.yaw * 3.14 / 180), 0)*walking_speed;
+      }else if(keys[SDL_SCANCODE_D]){
+        frame.camera_position = frame.camera_position + Vector(-sin(frame.yaw * 3.14 / 180), cos(frame.yaw * 3.14 / 180), 0)*walking_speed;
+      }
 
-    if(keys[SDL_SCANCODE_SPACE]){
-      speedz = 10;
+      if(keys[SDL_SCANCODE_SPACE]){
+        speedz = 10;
+      }
+
     }
 
     if(keys[SDL_SCANCODE_ESCAPE]){
