36a8ca8debe955d981f28cac07c2699b5c4191e4
M <gregor.alic@gimb.org>
2021-01-02 22:04:26 +0100

    Add support for older compilers

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ main.cpp libraries/raytracing/raytracing.cpp libraries/lodepng/lodepng.cpp -lSDL2 -pthread -o main.out
+g++ main.cpp libraries/raytracing/raytracing.cpp libraries/lodepng/lodepng.cpp -lSDL2 -pthread -o main.out -std=c++11
